51cd24a6722c7880c6e7c4ce616aaadc12f856d1
ARDA CAN CELIK <98707487+ARDACANCELIK@users.noreply.github.com>
2023-06-27 09:49:20 +0300

    added files

---
+++ 18-globalinstall.js
@@ -0,0 +1,7 @@
+// nodemon şu ana kadar dev dependency yüklü global instalu yok terminale nodemon app.js yazsan bulmuyor folderı vs çözüm global install şu anda local pacakge olarak var
+
+//npm install -g nodemon
+
+///yükledikten sonra bir dosyayı başlatmak için nodemon app.js demen gerekebilir.
+
+//npx kullanmak yahut local dependency kullanmak daha iyi smilgaya göre   npx=execute demek package runner yani
